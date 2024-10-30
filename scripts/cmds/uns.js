7fe4c1c56b12a2f3bfc83e3cfbe43707199c0cf6
NOMAN-TARIF <oss857726@gmail.com>
2024-10-30 20:10:41 +0600

    Create uns.js

---
+++ scripts/cmds/uns.js
@@ -0,0 +1,37 @@
+const { GoatWrapper } = require("fca-liane-utils");
+module.exports = {
+	config: {
+		name: "uns",
+		version: "1.2",
+		author: "NTKhang|ArYan 🤡",
+		countDown: 5,
+		role: 0,
+		description: {
+			vi: "",
+			en: "Unsend bot's message"
+		},
+		category: "box chat",
+		guide: {
+			vi: "reply tin nhắn muốn gỡ của bot và gọi lệnh {pn}",
+			en: "reply the message you want to unsend and call the command {pn}"
+		}
+	},
+
+	langs: {
+		vi: {
+			syntaxError: "Vui lòng reply tin nhắn muốn gỡ của bot"
+		},
+		en: {
+			syntaxError: "Please reply the message you want to unsend"
+		}
+	},
+
+	onStart: async function ({ message, event, api, getLang }) {
+		if (!event.messageReply || event.messageReply.senderID != api.getCurrentUserID())
+			return message.reply(getLang("syntaxError"));
+		message.unsend(event.messageReply.messageID);
+	}
+}
+
+const wrapper = new GoatWrapper(module.exports);
+wrapper.applyNoPrefix({ allowPrefix: true });
