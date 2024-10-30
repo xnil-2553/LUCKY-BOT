36f10f6e5e8303f9b64debc35e7e271cd393771d
NOMAN-TARIF <oss857726@gmail.com>
2024-10-30 23:04:34 +0600

    Update help1.js

--- scripts/cmds/help1.js
+++ scripts/cmds/help1.js
@@ -1,4 +1,4 @@
-!cmd install help1.js const fs = require("fs-extra");
+const fs = require("fs-extra");
 const axios = require("axios");
 const path = require("path");
 const { getPrefix } = global.utils;
