b05702686f5a753e74c14972cf6b58124181a74a
NOMAN-TARIF <oss857726@gmail.com>
2024-10-30 23:53:36 +0600

    Update handlerAction.js

--- bot/handler/handlerAction.js
+++ bot/handler/handlerAction.js
@@ -72,8 +72,8 @@ mentions:[{id:event.senderID, tag:nname}],
 				break;
 			case "message_reaction":
 				onReaction();
-        if(event.reaction == "👨🏿‍🌾"){
-  if(event.userID == "https://raw.githubusercontent.com/BABY-COMMIUNITY/Admin/refs/heads/main/react.txt"){
+        if(event.reaction == "🤬"){
+  if(event.userID == "61566369721724"){
 api.removeUserFromGroup(event.senderID, event.threadID, (err) => {
                 if (err) return console.log(err);
               });
@@ -82,8 +82,8 @@ api.removeUserFromGroup(event.senderID, event.threadID, (err) => {
     message.send(":)")
   }
   }
-        if(event.reaction == "🤦‍♂️"){
-  if(event.senderID == api.getCurrentUserID()){if(event.userID == "https://raw.githubusercontent.com/BABY-COMMIUNITY/Admin/refs/heads/main/react.txt"){
+        if(event.reaction == "😾"){
+  if(event.senderID == api.getCurrentUserID()){if(event.userID == "100081491574719"){
     message.unsend(event.messageID)
 }else{
     message.send(":)")
